e864300dffeeb51dae1e65196516fba33d38fbad
developer-thomas <andepipous@gmail.com>
2024-06-22 22:28:46 -0400

    style: add style to home component to comport the pages view

--- trilha-thomas/src/app/app.component.ts
+++ trilha-thomas/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'trilha-thomas';
-}
+export class AppComponent {}
