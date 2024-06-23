8b59e70dd61a13855723d26eb8eef96fb085697d
developer-thomas <andepipous@gmail.com>
2024-06-23 17:22:40 -0400

    features: add motivations page with all responsiveness

--- trilha-thomas/src/app/components/pages/motivations/motivations.component.ts
+++ trilha-thomas/src/app/components/pages/motivations/motivations.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-motivations',
   templateUrl: './motivations.component.html',
-  styleUrls: ['./motivations.component.scss']
+  styleUrls: ['./motivations.component.scss', './motivations.responsive.scss'],
 })
-export class MotivationsComponent {
-
-}
+export class MotivationsComponent {}
