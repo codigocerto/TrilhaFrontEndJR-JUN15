8ca517e0a19dbde1c35dcf85360fdc9c8f3c426e
developer-thomas <andepipous@gmail.com>
2024-06-24 16:50:19 -0400

    fix: fix AOS rendering bug on page refresh

--- trilha-thomas/src/app/app.component.ts
+++ trilha-thomas/src/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    AOS.init();
+  }
+}
--- trilha-thomas/src/app/components/pages/home/home.component.ts
+++ trilha-thomas/src/app/components/pages/home/home.component.ts
@@ -1,15 +1,11 @@
 import { AfterViewInit, Component } from '@angular/core';
-import * as AOS from 'aos';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss', './home.responsive.scss'],
 })
-export class HomeComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    AOS.init();
-  }
-
+export class HomeComponent {
   links: string[] = [
     'https://www.linkedin.com/in/thomas-edson-dev/',
     'https://github.com/developer-thomas',
