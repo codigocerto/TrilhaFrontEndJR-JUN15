be3981b2c97fbc8dc6c150e5f54d5b4daaa91dfc
developer-thomas <andepipous@gmail.com>
2024-06-28 12:23:08 -0400

    refactor: refactor interests ts. Move ownership of interest class information to a separate file. The logical about  render was also refactored

--- trilha-thomas/src/app/components/pages/interests/interests.component.ts
+++ trilha-thomas/src/app/components/pages/interests/interests.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { boxContent } from '../../../models/BoxContent.interface';
+import { sample_boxes } from '../../../data/interests-img';
 
 @Component({
   selector: 'app-interests',
@@ -7,68 +8,11 @@ import { boxContent } from '../../../models/BoxContent.interface';
   styleUrls: ['./interests.component.scss', './interests.responsive.scss'],
 })
 export class InterestsComponent {
-  content!: string | undefined;
-  image!: string | undefined;
+  boxes: boxContent[] = sample_boxes;
 
-  boxes: boxContent[] = [
-    {
-      id: 1,
-      icon: '../../../../assets/interests/icons/kung-fu-icon.png',
-      content: 'Sou Maai Saang Chong em Wing Chun e N3 em Sanda',
-      image: '../../../../assets/interests/content-img/kungfu-img.jpg',
-    },
-    {
-      id: 2,
-      icon: '../../../../assets/interests/icons/sing-icon.png',
-      content: 'Canto em casas de shows e sou vocalista de uma banda de rock',
-      image: '../../../../assets/interests/content-img/sing-img.jpg',
-    },
-    {
-      id: 3,
-      icon: '../../../../assets/interests/icons/xadrez-icon.png',
-      content: 'Jogo xadrez desde criança e já ganhei alguns campeonatos',
-      image: '../../../../assets/interests/content-img/xadrez-img.jpeg',
-    },
-    {
-      id: 4,
-      icon: '../../../../assets/interests/icons/videogame-icon.png',
-      content: 'Nas horas vagas jogo video-games, prefiro MMORPGS',
-      image: '../../../../assets/interests/content-img/videogame-img.jpg',
-    },
-    {
-      id: 5,
-      icon: '../../../../assets/interests/icons/travel-icon.png',
-      content: 'Destaque no curso de formação e ganhei uma viagem world-wide',
-      image: '../../../../assets/interests/content-img/travel-img.jpg',
-    },
-    {
-      id: 6,
-      icon: '../../../../assets/interests/icons/officer-icon.png',
-      content: 'Em 2018 ingressei na marinha por cuncurso público',
-      image: '../../../../assets/interests/content-img/officer-img.jpg',
-    },
-    {
-      id: 7,
-      icon: '../../../../assets/interests/icons/motorcycle-icon.png',
-      content: 'Gosto de pegar a estrada de moto e fazer trilhas em ramais',
-      image: '../../../../assets/interests/content-img/motor-img.jpg',
-    },
-    {
-      id: 8,
-      icon: '../../../../assets/interests/icons/futebol-icon.png',
-      content: 'Como goleiro participo de campeonatos de futebol',
-      image: '../../../../assets/interests/content-img/futebol-img.jpeg',
-    },
-    {
-      id: 9,
-      icon: '../../../../assets/interests/icons/cooker-icon.png',
-      content: 'Sou cozinheiro e já trabalhei em cozinhas industriais',
-      image: '../../../../assets/interests/content-img/cooker-img.jpg',
-    },
-  ];
+  contentObj!: boxContent;
 
-  teste(content: string | undefined, image: string | undefined) {
-    this.content = content;
-    this.image = image;
+  showContent(getContent: string, getImage: string, getAlt: string) {
+    this.contentObj = { content: getContent, image: getImage, alt: getAlt };
   }
 }
---
+++ trilha-thomas/src/app/data/interests-img.ts
@@ -0,0 +1,56 @@
+export const sample_boxes = [
+  {
+    alt: 'kungfu-img',
+    icon: '../../../../assets/interests/icons/kung-fu-icon.png',
+    content: 'Sou Maai Saang Chong em Wing Chun e N3 em Sanda',
+    image: '../../../../assets/interests/content-img/kungfu-img.jpg',
+  },
+  {
+    alt: 'sing-img',
+    icon: '../../../../assets/interests/icons/sing-icon.png',
+    content: 'Canto em casas de shows e sou vocalista de uma banda de rock',
+    image: '../../../../assets/interests/content-img/sing-img.jpg',
+  },
+  {
+    alt: 'xadrez-img',
+    icon: '../../../../assets/interests/icons/xadrez-icon.png',
+    content: 'Jogo xadrez desde criança e já ganhei alguns campeonatos',
+    image: '../../../../assets/interests/content-img/xadrez-img.jpeg',
+  },
+  {
+    alt: 'videogame-img',
+    icon: '../../../../assets/interests/icons/videogame-icon.png',
+    content: 'Nas horas vagas jogo video-games, prefiro MMORPGS',
+    image: '../../../../assets/interests/content-img/videogame-img.jpg',
+  },
+  {
+    alt: 'travel-img',
+    icon: '../../../../assets/interests/icons/travel-icon.png',
+    content: 'Destaquei no curso de formação e ganhei uma viagem world-wide',
+    image: '../../../../assets/interests/content-img/travel-img.jpg',
+  },
+  {
+    alt: 'military-img',
+    icon: '../../../../assets/interests/icons/officer-icon.png',
+    content: 'Em 2018 ingressei na marinha por cuncurso público',
+    image: '../../../../assets/interests/content-img/officer-img.jpg',
+  },
+  {
+    alt: 'motorcycle-img',
+    icon: '../../../../assets/interests/icons/motorcycle-icon.png',
+    content: 'Gosto de pegar a estrada de moto e fazer trilhas em ramais',
+    image: '../../../../assets/interests/content-img/motor-img.jpg',
+  },
+  {
+    alt: 'soccer-img',
+    icon: '../../../../assets/interests/icons/futebol-icon.png',
+    content: 'Como goleiro participo de campeonatos de futebol',
+    image: '../../../../assets/interests/content-img/futebol-img.jpeg',
+  },
+  {
+    alt: 'cooker-img',
+    icon: '../../../../assets/interests/icons/cooker-icon.png',
+    content: 'Sou cozinheiro e já trabalhei em cozinhas industriais',
+    image: '../../../../assets/interests/content-img/cooker-img.jpg',
+  },
+];
