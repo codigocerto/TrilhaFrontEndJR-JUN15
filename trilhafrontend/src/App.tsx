3a20d70ed6f7131afe9751c7da509a65ebfc09b1
Lindberg Pereira <lindbergpg@gmail.com>
2024-07-06 11:10:45 -0300

    configuracao inicial

---
+++ trilhafrontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Teste from './Test_1'
+import StyleGlobal from './styles'
+
+function App() {
+	return (
+		<>
+			<StyleGlobal />
+			<Teste />
+		</>
+	)
+}
+
+export default App
---
+++ trilhafrontend/src/styles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const StyleGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+  }
+`
+
+export default StyleGlobal
