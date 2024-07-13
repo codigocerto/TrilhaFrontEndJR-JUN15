8354ee34e9be1728e0c42ac24b62aa47c490bc71
Saulo Cavalcante Coimbra <121812117+SauloCoimbr@users.noreply.github.com>
2024-07-13 11:58:56 -0300

    feat:create a new session project and card component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import PersonalInfo from '../components/PersonalInfo';
 import About from '../components/About';
 import Contact from '../components/Contact';
+import Projects from '../components/Projects';
 
 const Home: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const Home: React.FC = () => {
         <PersonalInfo />
         <About />
         <Contact />
+        <Projects/>
       </main>
     </div>
   );
